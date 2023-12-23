9e1f2852b850bb3e03dbda3336a26a851467f4f3
Anastasiia Balan <90277333+anastasiiabalan@users.noreply.github.com>
2023-12-24 00:21:38 +0200

    Add files via upload

---
+++ client/src/data.js
@@ -0,0 +1,71 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://borisstudio.com/wp-content/webp-express/webp-images/uploads/2023/02/dizajn-dvuhkomnatnoj2-1.jpeg.webp",
+        title: "Однокімнатна",
+        desc: "КУПІВЛЯ",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://dim.lviv.ua/files/09-2018/ad2876/15382297501192883891_large.jpg",
+        title: "Трьохкімнатна",
+        desc: "ОРЕНДА",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://www.vorobievy-gory.kharkov.ua/wp-content/uploads/2021/09/3k1.jpg",
+        title: "Двохкімнатна",
+        desc: "ОБМІН",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories =[
+    {
+        id: 1,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIh09c0L3HrhpWOJT91cTmUzDgHNf88hWiUw&usqp=CAU",
+        title: "Купівля",
+        cat :"sale",
+    },
+    {
+        id: 2,
+        img: "https://board.mistaua.com/2020/98663_1_2.jpg",
+        title: "Оренда",
+        cat :"rent",
+    },
+    {
+        id: 3,
+        img: "https://ireland.apollo.olxcdn.com/v1/files/1dvhm7ksqpkz2-UA/image;s=1400x933",
+        title: "Обмін",
+        cat :"exchange",
+    },
+];
+
+export const popularProducts =[
+    {
+        id: 1,
+        img: "https://konkurent.ua/media/uploads/prev/2018/10/11/16/52/55/7e08bc73cd3cb4c3ff1f859259789c94.png",
+    },
+    {
+        id: 2,
+        img: "https://manhattan-up.com.ua/wp-content/uploads/2023/05/1200x700.jpg.webp",
+    },
+    {
+        id: 3,
+        img: "https://img.rieltor.ua/crop/400x300/offers/580/80/0/1775572889527580.jpg",
+    },
+    {
+        id: 4,
+        img: "https://www.rbc.ua/static/img/s/c/screenshot_2_495_1200x675.jpg",
+    },
+    {
+        id: 5,
+        img: "https://viyarbazar.com/upload/sprint.editor/7e2/7e2ae08744e437dac7f779c4dadd18b5.jpg",
+    },
+    {
+        id: 6,
+        img: "https://www.remontov.kiev.ua/wp-content/uploads/2019/10/4b9644d0-f4e1-4166-b036-4c02bbd012a6-1.jpg",
+    },
+];
