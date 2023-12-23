13a1b583c4cefef6b060fa03e5d58de70995e829
Anastasiia Balan <90277333+anastasiiabalan@users.noreply.github.com>
2023-12-24 00:21:15 +0200

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Main from "./ChatComponents/Main";
+import Chat from "./ChatComponents/Chat";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+
+  const routes = [
+    { path: "/", element: <Home /> },
+    { path: "/products/:category", element: <ProductList /> },
+      { path: "/products", element: <ProductList /> },      ,
+      { path: "/product/:id", element: <Product /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/login", element: <Login /> },
+      {path: "/main", element: <Main />},
+     {path: "/chat", element: <Chat />},
+  ];
+  return (
+      <BrowserRouter>
+        <Routes>
+          { routes.map((item, index) =>
+              <Route key={index} path={item.path} element={item.element} /> )
+          }
+        </Routes>
+      </BrowserRouter>
+
+  )
+}
+
+export default App;
