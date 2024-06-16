4421bfba4eba17f1ba9948f1aa8a34140814c204
Anastasiia Balan <90277333+anastasiiabalan@users.noreply.github.com>
2024-06-16 21:18:18 +0300

    Update data.js

--- client/src/data.js
+++ client/src/data.js
@@ -1,23 +1,23 @@
 export const sliderItems = [
     {
         id: 1,
-        img: "https://borisstudio.com/wp-content/webp-express/webp-images/uploads/2023/02/dizajn-dvuhkomnatnoj2-1.jpeg.webp",
-        title: "Однокімнатна",
-        desc: "КУПІВЛЯ",
+        img: "https://i.ytimg.com/vi/-cvLm3pv7xw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBJpFW7PDQmdb1qdA31gB7Zto4aUw",
+        title: "Гердани",
+        desc: "Вироби з чеського бісеру",
         bg: "f5fafd",
     },
     {
         id: 2,
-        img: "https://dim.lviv.ua/files/09-2018/ad2876/15382297501192883891_large.jpg",
-        title: "Трьохкімнатна",
-        desc: "ОРЕНДА",
+        img: "https://idei-dekoru.com/wp-content/uploads/2017/04/791c355494337b6ffdd093d94988de9e.jpg",
+        title: "Полімерна глина",
+        desc: "Можливість індивідуального замовлення",
         bg: "fcf1ed",
     },
     {
         id: 3,
-        img: "https://www.vorobievy-gory.kharkov.ua/wp-content/uploads/2021/09/3k1.jpg",
-        title: "Двохкімнатна",
-        desc: "ОБМІН",
+        img: "https://cash-flow.com.ua/wp-content/uploads/podelki-iz-dereva-svoimi-rukami-45.jpg",
+        title: "Вироби з дерева",
+        desc: "Вироби під ваші заміри",
         bg: "fbf0f4",
     },
 ];
@@ -25,20 +25,20 @@ export const sliderItems = [
 export const categories =[
     {
         id: 1,
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIh09c0L3HrhpWOJT91cTmUzDgHNf88hWiUw&usqp=CAU",
-        title: "Купівля",
+        img: "https://www.useek.com.ua/images/users/2023-94-751-photo_2023-04-03_19-47-57.jpg",
+        title: "Бісер",
         cat :"sale",
     },
     {
         id: 2,
-        img: "https://board.mistaua.com/2020/98663_1_2.jpg",
-        title: "Оренда",
+        img: "https://images.prom.ua/5527289680_nabir-obrobnih-doshok.jpg",
+        title: "Дерево",
         cat :"rent",
     },
     {
         id: 3,
-        img: "https://ireland.apollo.olxcdn.com/v1/files/1dvhm7ksqpkz2-UA/image;s=1400x933",
-        title: "Обмін",
+        img: "https://lihtaryk.com.ua/wp-content/uploads/2018/03/claude-monet-vpechatlenie-voshod-solnca.jpg",
+        title: "Картини",
         cat :"exchange",
     },
 ];
@@ -46,26 +46,28 @@ export const categories =[
 export const popularProducts =[
     {
         id: 1,
-        img: "https://konkurent.ua/media/uploads/prev/2018/10/11/16/52/55/7e08bc73cd3cb4c3ff1f859259789c94.png",
+        img: "https://ireland.apollo.olxcdn.com/v1/files/of8m1rxk6p3p2-UA/image;s=3994x4082",
     },
     {
         id: 2,
-        img: "https://manhattan-up.com.ua/wp-content/uploads/2023/05/1200x700.jpg.webp",
+        img: "https://ireland.apollo.olxcdn.com/v1/files/s4eq5ps5avp63-UA/image;s=1632x1632",
     },
     {
         id: 3,
-        img: "https://img.rieltor.ua/crop/400x300/offers/580/80/0/1775572889527580.jpg",
+        img: "https://optim.tildacdn.com/stor6237-6161-4733-b033-633763333834/-/format/webp/77801552.jpg",
     },
     {
         id: 4,
-        img: "https://www.rbc.ua/static/img/s/c/screenshot_2_495_1200x675.jpg",
+        img: "https://images.prom.ua/1877681452_w600_h600_1877681452.jpg",
     },
     {
         id: 5,
-        img: "https://viyarbazar.com/upload/sprint.editor/7e2/7e2ae08744e437dac7f779c4dadd18b5.jpg",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz0zZu8n0BkemkWv3T_dpumTbY4hUXKlxIHrKf6r9FCCfIpwoyn6beoNQoxRYUXyUkk2k&usqp=CAU",
     },
     {
         id: 6,
-        img: "https://www.remontov.kiev.ua/wp-content/uploads/2019/10/4b9644d0-f4e1-4166-b036-4c02bbd012a6-1.jpg",
+        img: "https://www.uagallery.com.ua/files/categories/xpeizag.jpg.pagespeed.ic.8D_4J7ir71.jpg",
     },
+
+
 ];
